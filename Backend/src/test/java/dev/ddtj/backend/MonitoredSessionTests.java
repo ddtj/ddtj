8605c7adaa215759313a6b01ab8d7cf0f705ba46
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2021-12-20 22:11:17 +0200

    Again, coverage improvement, ugh

--- Backend/src/test/java/dev/ddtj/backend/MonitoredSessionTests.java
+++ Backend/src/test/java/dev/ddtj/backend/MonitoredSessionTests.java
@@ -17,10 +17,15 @@
  */
 package dev.ddtj.backend;
 
+import com.sun.jdi.AbsentInformationException;
+import com.sun.jdi.LocalVariable;
 import com.sun.jdi.Method;
 import com.sun.jdi.ReferenceType;
 import com.sun.jdi.VirtualMachine;
 import dev.ddtj.backend.javadebugger.MonitoredSession;
+import java.util.Arrays;
+import java.util.List;
+import lombok.extern.java.Log;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,6 +34,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
+@Log
 class MonitoredSessionTests {
     @Mock
     private VirtualMachine virtualMachine;
@@ -37,18 +43,36 @@ class MonitoredSessionTests {
     private Method method;
 
     @Mock
+    private LocalVariable localVariable;
+
+    @Mock
     private ReferenceType referenceType;
 
     private static final String DECLARING_CLASS = "test.MyTestClass";
     private static final String METHOD_SIGNATURE = "testMethodName()";
 
     @Test
-    void enteringMethodTest() {
+    void enteringMethodTest() throws AbsentInformationException {
+        MonitoredSession session = initSession();
+        Mockito.when(method.arguments()).thenReturn(List.of(localVariable));
+        Mockito.when(localVariable.name()).thenReturn("testArg");
+        Mockito.when(localVariable.typeName()).thenReturn("int");
+        Assertions.assertSame(session.enteringMethod(method), session.enteringMethod(method));
+    }
+
+    @Test()
+    void absentInformationExceptionTest() throws AbsentInformationException {
+        MonitoredSession session = initSession();
+        Mockito.when(method.arguments()).thenThrow(new AbsentInformationException());
+        log.severe("There should be a stack trace for AbsentInformationException, this is valid and part of the test");
+        Assertions.assertSame(session.enteringMethod(method), session.enteringMethod(method));
+    }
+
+    private MonitoredSession initSession() {
         MonitoredSession session = new MonitoredSession(virtualMachine, "test.*");
         Mockito.when(referenceType.name()).thenReturn(DECLARING_CLASS);
         Mockito.when(method.declaringType()).thenReturn(referenceType);
         Mockito.when(method.signature()).thenReturn(METHOD_SIGNATURE);
-
-        Assertions.assertSame(session.enteringMethod(method), session.enteringMethod(method));
+        return session;
     }
 }
