18cbeb668bb32d08d757272e2b2f5942a1078327
Shai Almog <67850168+shai-almog@users.noreply.github.com>
2021-12-28 15:27:11 +0200

    Improved coverage

--- Backend/src/test/java/dev/ddtj/backend/DataCollectorTests.java
+++ Backend/src/test/java/dev/ddtj/backend/DataCollectorTests.java
@@ -3,19 +3,22 @@ package dev.ddtj.backend;
 import com.sun.jdi.IncompatibleThreadStateException;
 import com.sun.jdi.Location;
 import com.sun.jdi.Method;
-import com.sun.jdi.ObjectReference;
 import com.sun.jdi.ReferenceType;
 import com.sun.jdi.StackFrame;
+import com.sun.jdi.StringReference;
 import com.sun.jdi.ThreadReference;
 import com.sun.jdi.event.MethodEntryEvent;
+import com.sun.jdi.event.MethodExitEvent;
 import com.sun.jdi.event.StepEvent;
 import com.sun.jdi.event.VMDeathEvent;
+import dev.ddtj.backend.data.ExecutionState;
+import dev.ddtj.backend.data.Invocation;
 import dev.ddtj.backend.data.ParentMethod;
 import dev.ddtj.backend.data.objectmodel.BaseType;
+import dev.ddtj.backend.data.objectmodel.BuiltinTypes;
 import dev.ddtj.backend.javadebugger.DataCollector;
 import dev.ddtj.backend.javadebugger.MonitoredSession;
 import java.util.Collections;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -31,7 +34,7 @@ class DataCollectorTests {
     private MethodEntryEvent methodEntryEvent;
 
     @Mock
-    private StepEvent stepEvent;
+    private MethodExitEvent methodExitEvent;
 
     @Mock
     private MonitoredSession monitoredSession;
@@ -54,12 +57,16 @@ class DataCollectorTests {
     @Mock
     private Location location;
 
+    @Mock
+    private StringReference stringValue;
+
     @Test
     void testProcessEvent() throws IncompatibleThreadStateException {
         DataCollector dataCollector = new DataCollector();
         dataCollector.processEvent(monitoredSession, vmDeathEvent, 1);
 
         Mockito.when(methodEntryEvent.method()).thenReturn(method);
+        Mockito.when(methodExitEvent.method()).thenReturn(method);
         Mockito.when(method.declaringType()).thenReturn(declaringType);
         Mockito.when(declaringType.isPublic()).thenReturn(true);
         Mockito.when(method.isConstructor()).thenReturn(false);
@@ -68,11 +75,19 @@ class DataCollectorTests {
         Mockito.when(method.isPrivate()).thenReturn(false);
         Mockito.when(method.isProtected()).thenReturn(false);
         Mockito.when(method.isPackagePrivate()).thenReturn(false);
+        Mockito.when(parentMethod.getReturnValue()).thenReturn(BuiltinTypes.STRING);
         Mockito.when(declaringType.name()).thenReturn("com.company.ClassName");
         Mockito.when(monitoredSession.getOrCreateMethod(method)).thenReturn(parentMethod);
+        ExecutionState executionState = new ExecutionState();
+        executionState.setParentMethod(parentMethod);
+        executionState.setInvocation(new Invocation());
+
+        Mockito.when(monitoredSession.removeExecutionState(methodExitEvent)).thenReturn(executionState);
         Mockito.when(parentMethod.getParameters()).thenReturn(new BaseType[0]);
 
         Mockito.when(methodEntryEvent.thread()).thenReturn(threadReference);
+        Mockito.when(methodExitEvent.returnValue()).thenReturn(stringValue);
+        Mockito.when(stringValue.value()).thenReturn("returnValue");
         Mockito.when(threadReference.frameCount()).thenReturn(1);
         Mockito.when(threadReference.frame(0)).thenReturn(stackFrame);
         Mockito.when(stackFrame.location()).thenReturn(location);
@@ -80,6 +95,10 @@ class DataCollectorTests {
         Mockito.when(location.method()).thenReturn(method);
 
         dataCollector.processEvent(monitoredSession, methodEntryEvent, 1);
+        Mockito.verify(monitoredSession, Mockito.times(2))
+                .getOrCreateMethod(method);
+
+        dataCollector.processEvent(monitoredSession, methodExitEvent, 1);
         Mockito.verify(monitoredSession, Mockito.times(2))
                 .getOrCreateMethod(method);
     }
